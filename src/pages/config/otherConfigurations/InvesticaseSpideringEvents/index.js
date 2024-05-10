909b403f5f86339d2a1471f273584699e85d1750
srujanaj313 <srujanaj313@gmail.com>
2024-05-10 14:24:06 +0530

    'investicase_view_changes'

--- src/pages/config/otherConfigurations/InvesticaseSpideringEvents/index.js
+++ src/pages/config/otherConfigurations/InvesticaseSpideringEvents/index.js
@@ -509,8 +509,7 @@ export default function InvesticaseSpideringEvents() {
                             >
                               {row.editFlag === true && (
                                 <Tooltip title="Edit" placement="left">
-                                  {/* <IconButton disabled={!isUpdateAccessExist()}> */}
-                                  <IconButton disabled={false}>
+                                  <IconButton disabled={!isUpdateAccessExist()}>
                                     <EditNoteIcon
                                       sx={{ cursor: "pointer" }}
                                       fontSize="medium"
