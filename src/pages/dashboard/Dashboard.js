f4ff34db18c7850d35753a25b9c08a6c18f30967
Vid <vid@gmail.com>
2024-07-15 20:29:12 +0530

    height adjusted

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -7,12 +7,13 @@ import CalendarView from "./calendarview/CalendarView";
 
 const Dashboard = () => {
   return (
-    <Box >
-      <Grid container sx={{ border: '2px solid #000' }}>
+    <Box sx={{ marginTop: '45px'}}>
+      {/* <NavBar /> */}
+      <Grid container sx={{ height: 'calc(100vh - 77px)', border: '2px solid #000' }}>
         <Grid item xs={12} sm={4} xl={3}>
           <PerformanceMetrics />
         </Grid>
-        <Grid item xs={12} sm={8} xl={9} >
+        <Grid item xs={12} sm={8} xl={9} maxHeight={'100%'} overflow={'auto'}>
           {/* <Box style={{ maxHeight: '100%', overflow: 'auto' }}> */}
             {/* <CaseloadMetrics />*/}
             <CalendarView />
